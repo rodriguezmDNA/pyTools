488fe5e0d997716f74df9cfe4cf4bb70157f276f
rodriguezmDNA <joelrome88@gmail.com>
2020-12-08 19:14:26 -0800

    trying xargs

---
+++ notebooks/test_xargs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file=$@ 
+mkdir -p 'out'
+
+fname=$(basename $file)
+basefile=${fname%'.txt'}
+touch 'out/'$basefile'_processed.txt'
+
+
+#tail -n +2 $file | awk 'BEGIN {FS="\t"; print "ID","sepArea","petArea"} {print $1,$2*$3,$4*$5}' > 'out/'$basefile'_processed.txt'
