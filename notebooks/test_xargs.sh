1361df7645899c6e8fe1114ecf82a895311c6bf2
rodriguezmDNA <joelrome88@gmail.com>
2020-12-09 22:09:23 -0800

    work on decorators

--- notebooks/test_xargs.sh
+++ notebooks/test_xargs.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-file=$@ 
-mkdir -p 'out'
-
-fname=$(basename $file)
-basefile=${fname%'.txt'}
-touch 'out/'$basefile'_processed.txt'
-
-
-#tail -n +2 $file | awk 'BEGIN {FS="\t"; print "ID","sepArea","petArea"} {print $1,$2*$3,$4*$5}' > 'out/'$basefile'_processed.txt'
+#mkdir -p out
+#tail -n +2 $file | xargs awk 'BEGIN {FS="\t"; print "ID","sepArea","petArea"} {print $1,$2*$3,$4*$5}'
+awk 'BEGIN {FS="\t"; print "ID","sepArea","petArea"} {print $1,$2*$3,$4*$5\n}'
