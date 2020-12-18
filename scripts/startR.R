26c13470e35d5d9c769cc18c00b288fc0c8007ef
rodriguezmDNA <joelrome88@gmail.com>
2020-12-17 19:22:43 -0600

    add venn diagram viz

--- scripts/startR.R
+++ scripts/startR.R
@@ -1,5 +1,9 @@
 install.packages("pheatmap")
 install.packages("UpSetR")
 install.packages("ggrepel")
+install.packages("BiocManager")
 
+###
+BiocManager::install(c("RBGL","graph"))
+install.packages("Vennerable", repos="http://R-Forge.R-project.org")
 
